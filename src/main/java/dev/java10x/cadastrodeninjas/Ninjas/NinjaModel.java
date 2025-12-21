aefdf4e59880c8ca962258d17afe009bd28d6031
Mateus Santana <mateusantana@proton.me>
2025-12-21 14:13:09 -0300

    fix: add @Collumn in class

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -20,11 +20,13 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
     //Um ninja tem uma unica missao
