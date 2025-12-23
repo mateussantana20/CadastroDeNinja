b2aec4fcc995b69ffb20df2b7384d065f47346f5
Mateus Santana <mateusantana@proton.me>
2025-12-23 16:03:21 -0300

    feat: Alterar rota do controler para listar os ninjas

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,9 +1,17 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/boasvindas")
     public String boasvindas () {
         return "Boas vindas";
@@ -12,13 +20,13 @@ public class NinjaController {
     // Adicionar ninja - CREATE
     @PostMapping("/criar")
     public String criarNinja () {
-        return "Ninja Criada";
+        return "Criandop Ninja";
     }
 
     // Mostrar todos os ninjas - READ
     @GetMapping("/listar")
-    public String mostrarTodosOsNinjas () {
-        return "Mostrando todos os ninjas";
+    public List<NinjaModel> listarNinjas () {
+        return ninjaService.listarNinjas();
     }
 
     // Mostrar ninja por ID  - READ
