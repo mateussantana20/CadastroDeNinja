9191c6b28701fff89a0e4be178ff864280656d8e
Mateus Santana <mateusantana@proton.me>
2025-12-23 19:51:52 -0300

    feat: Incluindo a rota de cadastro de ninjas usando metodo POST

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -19,8 +19,8 @@ public class NinjaController {
 
     // Adicionar ninja - CREATE
     @PostMapping("/criar")
-    public String criarNinja () {
-        return "Criandop Ninja";
+    public NinjaModel criarNinja (@RequestBody NinjaModel ninja) {
+        return ninjaService.criarNinja(ninja);
     }
 
     // Mostrar todos os ninjas - READ
