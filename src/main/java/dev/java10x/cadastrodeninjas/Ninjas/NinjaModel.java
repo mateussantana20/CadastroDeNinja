f803ca2dd4a8147a0420c6673a731f49291ebd09
Mateus Santana <mateusantana@proton.me>
2025-12-24 20:12:11 -0300

    fix: Inclui a coluna de rank na entidade

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -30,6 +30,9 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "rank")
+    private String rank;
+
     //Um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foreing key
